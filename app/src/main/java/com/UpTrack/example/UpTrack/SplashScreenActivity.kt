a4cb9c67791d7d007e6a89d4744d18c8dcc035bd
davidkiefel <davidkiefel@gmail.com>
2023-07-07 21:50:18 +1000

    Added Splash screen and background image

---
+++ app/src/main/java/com/UpTrack/example/UpTrack/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.UpTrack.example.UpTrack
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 1000 // 3 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTimeOut)
+    }
+}
